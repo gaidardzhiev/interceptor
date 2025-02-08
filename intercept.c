872e7704e00cc72a7a592dc87072e49c96681930
gaidardzhiev <1v4n@protonmail.com>
2025-02-08 08:40:46 +0000

    Sat Feb  8 08:40:46 AM UTC 2025

--- intercept.c
+++ intercept.c
@@ -12,24 +12,22 @@
 #include <arpa/inet.h>
 #include <netdb.h>
 
-#define BUFFER_LEN 8192
+#define BUF 8192
 
-int i(int s, const struct sockaddr *a, socklen_t addrlen) {
+int i(int s, const struct sockaddr *a, socklen_t z) {
 	int (*c)(int,const struct sockaddr*, socklen_t) = NULL;
 	c = dlsym(RTLD_NEXT, "connect");
 	struct sockaddr_in *myaddr = (struct sockaddr_in*)a;
 	char *fam = "OTHER";
 	if (a->sa_family == AF_INET)
 		fam = "AF_INET";
-	char address_str[BUFFER_LEN] = {0};
+	char address_str[BUF] = {0};
 	if (a->sa_family == AF_INET) {
-		inet_ntop(a->sa_family, &(myaddr->sin_addr.s_addr), address_str, BUFFER_LEN);
+		inet_ntop(a->sa_family, &(myaddr->sin_addr.s_addr), address_str, BUF);
 	}
-	char host[BUFFER_LEN] = {0};
-	char server[BUFFER_LEN] = {0};
-	getnameinfo(a, addrlen,
-		    host, BUFFER_LEN,
-		    server, BUFFER_LEN, 0);
-	printf("%s (%s) %s\n",host, fam, address_str);
-	return c(s, a, addrlen);
+	char host[BUF] = {0};
+	char server[BUF] = {0};
+	getnameinfo(a,z,host,BUF,server,BUF,0);
+	printf("%s (%s) %s\n",host,fam,address_str);
+	return c(s,a,z);
 }
